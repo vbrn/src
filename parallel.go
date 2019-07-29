3d646b94e7ba74d101a1d2a5a3e47d148bc77f30
user <you@example.com>
2019-07-29 15:55:42 +0300

    parallel added

---
+++ parallel.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+	"time"
+)
+
+var wg sync.WaitGroup
+
+func init() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+}
+
+func main() {
+	wg.Add(2)
+	go foo()
+	go bar()
+	wg.Wait()
+}
+
+func foo() {
+	for i := 0; i < 4; i++ {
+		fmt.Println("Foo:", i)
+		time.Sleep(3 * time.Millisecond)
+	}
+	wg.Done()
+}
+
+func bar() {
+	for i := 10; i < 14; i++ {
+		fmt.Println("Bar:", i)
+		time.Sleep(6 * time.Millisecond)
+	}
+	wg.Done()
+}
