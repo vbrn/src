326737d1e25189f1c2f7c3673c963603ab1fe191
user <you@example.com>
2019-08-02 22:37:59 +0300

    abra 2

--- tour2.go
+++ tour2.go
@@ -3,8 +3,32 @@ package main
 import (
     "fmt"
     "math"
+    "runtime"
     
 )
+
+
+
+
+
+   //IF WITH A STATEMENT
+func pow(x, n, lim float64) float64 {
+	if v := math.Pow(x, n); v < lim {return v} else {fmt.Printf("%g >= %g\n", v, lim)
+	}	// can't use v here, though
+	    return lim}
+	    
+func Sqrt(x float64) float64 {
+	for z := float64(int(x/2)); z > 0; z-- {
+ 		formula := (z*z - x)/(2*z)
+ 			if formula == 0 {
+ 				return z } } 
+		return 0.0 }
+
+
+
+
+
+
 
 func main() {
     //FOR
@@ -23,6 +47,14 @@ func main() {
         if sum < 0 { break}
         fmt.Println(sum)
         sum = sum - 5 }
+        
+    //GOTO
+    var x3 = 15
+    LOOP: if x3> 0 {
+    fmt.Println("goto loop", x3)
+    x3--
+    goto LOOP }
+    
     //IF WITH A STATEMENT
  	fmt.Println(
 		pow(3, 2, 10),
@@ -33,17 +65,11 @@ func main() {
 	if x1 == 3 && y1 > 0 { fmt.Println("even")} 
     fmt.Println("selfsqrt", Sqrt(9))
 
-}
-
-   //IF WITH A STATEMENT
-func pow(x, n, lim float64) float64 {
-	if v := math.Pow(x, n); v < lim {return v} else {fmt.Printf("%g >= %g\n", v, lim)
-	}	// can't use v here, though
-	    return lim}
-	    
-func Sqrt(x float64) float64 {
-	for z := float64(int(x/2)); z > 0; z-- {
- 		formula := (z*z - x)/(2*z)
- 			if formula == 0 {
- 				return z } } 
-		return 0.0 }
+//SWITCH
+fmt.Println("Го ранз он")
+switch os := runtime.GOOS; os {
+case "darwin": fmt.Println("OS X")
+case "linux": fmt.Printf("Linux %s", os)
+default: //freebsd openbsd plan9 windows
+    fmt.Printf("%s, \n", os) }
+	}
